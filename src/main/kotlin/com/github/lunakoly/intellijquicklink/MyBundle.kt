3aa085c26fe0ff5000ed66ff9717a234b42fc6b3
GitHub Action <action@github.com>
2021-07-22 14:16:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/lunakoly/intellijquicklink/MyBundle.kt
+++ src/main/kotlin/com/github/lunakoly/intellijquicklink/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lunakoly.intellijquicklink
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/lunakoly/intellijquicklink/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lunakoly.intellijquicklink.services
+
+import com.github.lunakoly.intellijquicklink.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
