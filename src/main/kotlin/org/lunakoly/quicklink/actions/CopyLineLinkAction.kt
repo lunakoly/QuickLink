fe6756aa79854ce92f01602fb72755d9122630ca
Nikolay Lunyak <lunyak.kolya@mail.ru>
2024-09-25 22:55:23 +0300

    🔥 Pick the closest reasonable commit if the current one was not pushed
    After this change there are now 3 actions:
    
    - generating a link to the currently checked out commit
    - generating a link to the latest commit from the remote's default branch
    - generating a link to some parent commit that is definitely available at the remote
    
    ...with the last one becoming the default assigned to `Cmd`+`Shift`+`L`.
    
    The main use cases are generating a link to whatever I see in my IDE (so,
    close commits tend to also be acceptable since the code hasn't changed
    much) or showing something that is currently in the latest master. Also, I
    expect there to be not that many users relying on the plugin generating
    a link to exactly the current commit in scenarios when it has not been
    pushed, thought I don't really have any statistics. I assume no one has
    tried automating anything by calling the action or the default shortcut.

--- src/main/kotlin/org/lunakoly/quicklink/actions/CopyLineLinkAction.kt
+++ src/main/kotlin/org/lunakoly/quicklink/actions/CopyLineLinkAction.kt
@@ -4,19 +4,48 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vfs.VfsUtilCore
-import org.lunakoly.quicklink.repository.getRepositoryInfo
-import org.lunakoly.quicklink.ui.warn
+import git4idea.repo.GitRepository
+import org.lunakoly.quicklink.repository.*
 import org.lunakoly.quicklink.ui.showClickableListIfNeeded
 import org.lunakoly.quicklink.ui.toast
+import org.lunakoly.quicklink.ui.warn
 import org.lunakoly.quicklink.urlbuilder.LineOffset
 import org.lunakoly.quicklink.urlbuilder.Selection
 import org.lunakoly.quicklink.urlbuilder.UrlBuilders
 import org.lunakoly.quicklink.utils.PopupException
 import org.lunakoly.quicklink.utils.catchingPopupExceptions
+import org.lunakoly.quicklink.utils.runInBackground
 import org.lunakoly.quicklink.utils.toDomain
 import java.awt.datatransfer.StringSelection
+
+class CopyLineLinkAction : DumbAwareAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        catchingPopupExceptions {
+            generateLineLink(event, ::getReasonablePublishedRepositoryInfo)
+        }
+    }
+}
+
+class CopyCurrentCommitLineLinkAction : DumbAwareAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        catchingPopupExceptions {
+            generateLineLink(event) { repo, _, _ ->
+                getCurrentCommitRepositoryInfo(repo)
+            }
+        }
+    }
+}
+
+class CopyLatestDefaultLineLinkAction : DumbAwareAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        catchingPopupExceptions {
+            generateLineLink(event, ::getLatestDefaultRepositoryInfo)
+        }
+    }
+}
 
 class NoProjectException : PopupException(
     "Please, open a project",
@@ -38,61 +67,57 @@ class RelativePathException : PopupException(
     "No Relative Path",
 )
 
-class CopyLineLinkAction : DumbAwareAction() {
-    @Suppress("ThrowsCount")
-    private fun generateLineLink(event: AnActionEvent) {
-        val project = event.project
-            ?: throw NoProjectException()
-        val editor = event.getData(CommonDataKeys.EDITOR)
-            ?: throw NoEditorException()
-        val currentFile = event.dataContext.getData(CommonDataKeys.VIRTUAL_FILE)?.canonicalFile
-            ?: throw NoActiveFileException()
+private inline fun generateLineLink(
+    event: AnActionEvent,
+    crossinline getInfo: (GitRepository, Project, String) -> RepositoryInfo,
+) {
+    val project = event.project
+        ?: throw NoProjectException()
+    val editor = event.getData(CommonDataKeys.EDITOR)
+        ?: throw NoEditorException()
+    val currentFile = event.dataContext.getData(CommonDataKeys.VIRTUAL_FILE)?.canonicalFile
+        ?: throw NoActiveFileException()
 
-        val selection = if (editor.selectionModel.hasSelection()) {
-            val startLine = 1 + editor.document.getLineNumber(editor.selectionModel.selectionStart)
-            val endLine = 1 + editor.document.getLineNumber(editor.selectionModel.selectionEnd)
-            val startColumn = 1 + editor.selectionModel.selectionStartPosition!!.getColumn()
-            val endColumn = 1 + editor.selectionModel.selectionEndPosition!!.getColumn()
-            val startOffset = LineOffset(startLine, startColumn)
-            val endOffset = LineOffset(endLine, endColumn)
-            Selection.MultilineSelection(startOffset, endOffset)
-        } else {
-            val lineOffset = LineOffset(1 + editor.document.getLineNumber(editor.caretModel.offset), 0)
-            Selection.SingleLinkSelection(lineOffset)
-        }
+    val currentFileIsModified = ChangeListManager
+        .getInstance(project)
+        .getChange(currentFile) != null
 
-        val repositoryInfo = getRepositoryInfo(project, currentFile)
+    if (currentFileIsModified) {
+        project.warn("This is a locally modified file, the line number may be inaccurate")
+    }
 
-        val filePath = VfsUtilCore
-            .findRelativePath(repositoryInfo.root, currentFile, VfsUtilCore.VFS_SEPARATOR_CHAR)
-            ?: throw RelativePathException()
+    val selection = if (editor.selectionModel.hasSelection()) {
+        val startLine = 1 + editor.document.getLineNumber(editor.selectionModel.selectionStart)
+        val endLine = 1 + editor.document.getLineNumber(editor.selectionModel.selectionEnd)
+        val startColumn = 1 + editor.selectionModel.selectionStartPosition!!.getColumn()
+        val endColumn = 1 + editor.selectionModel.selectionEndPosition!!.getColumn()
+        val startOffset = LineOffset(startLine, startColumn)
+        val endOffset = LineOffset(endLine, endColumn)
+        Selection.MultilineSelection(startOffset, endOffset)
+    } else {
+        val lineOffset = LineOffset(1 + editor.document.getLineNumber(editor.caretModel.offset), 0)
+        Selection.SingleLinkSelection(lineOffset)
+    }
 
-        val currentFileIsModified = ChangeListManager
-            .getInstance(project)
-            .getChange(currentFile) != null
+    val repository = getRepositoryFor(currentFile, project)
+    val remotesMap = repository.getRemotesMap()
 
-        if (currentFileIsModified) {
-            project.warn("This is a locally modified file, the line number may be inaccurate")
-        }
+    editor.showClickableListIfNeeded(things = remotesMap.keys.toList(), title = "Select a Remote") { remote ->
+        project.runInBackground("Generating the line link") {
+            val repositoryInfo = getInfo(repository, project, remote)
 
-        editor.showClickableListIfNeeded(
-            repositoryInfo.remotesNames,
-            title = "Select a Remote",
-        ) {
-            val remoteLink = repositoryInfo.remotes[it]
-                ?: return@showClickableListIfNeeded
+            val filePath = VfsUtilCore
+                .findRelativePath(repositoryInfo.root, currentFile, VfsUtilCore.VFS_SEPARATOR_CHAR)
+                ?: throw RelativePathException()
+
+            val remoteLink = remotesMap[remote]
+                ?: return@runInBackground
 
             val urlBuilder = UrlBuilders.fromDomain(remoteLink.toDomain())
             val url = urlBuilder.buildUrl(remoteLink, repositoryInfo, filePath, selection)
 
             CopyPasteManager.getInstance().setContents(StringSelection(url))
             project.toast("Line link copied: $url")
-        }
-    }
-
-    override fun actionPerformed(event: AnActionEvent) {
-        catchingPopupExceptions {
-            generateLineLink(event)
         }
     }
 }
