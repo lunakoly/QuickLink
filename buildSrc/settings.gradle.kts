97594e0eadb809effb38a78637e656fe66a08d5c
Nikolay Lunyak <lunyak.kolya@mail.ru>
2024-09-30 17:38:56 +0300

    🛠️ Downgrade Qodana & add `foojay-resolver-convention` to `:buildSrc`
    Downgrading fixes:
    
    > java.io.IOException: Exception /opt/idea/plugins/sh/lib/sh.jar while loading META-INF/sh.xml
    
    And `foojay-resolver-convention` fixes:
    
    > Cannot find a Java installation on your machine matching this tasks requirements:
    > {languageVersion=11, vendor=any, implementation=vendor-specific} for LINUX on x86_64.

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention")
+}
+
 // Source:
 // https://docs.gradle.org/current/userguide/platforms.html#sec:importing-catalog-from-file
 dependencyResolutionManagement {
